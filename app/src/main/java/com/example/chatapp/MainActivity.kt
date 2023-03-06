8f31c629ebee9e6a5a8087727878b72b660ab721
DudeNamedVictor <greatgomer3@gmail.com>
2023-03-06 16:15:42 +0300

    Add check internet

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.chatapp
 
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.utils.ConnectionLiveData
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +20,31 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navHostFragment.navController
+        checkInternet()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun checkInternet() {
+        val connectivityManager =
+            application.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectionLiveData =
+            ConnectionLiveData(connectivityManager)
+        connectionLiveData.observe(this) { isNetworkAvailable ->
+            isNetworkAvailable?.let {
+                updateUI(it)
+            }
+        }
+    }
+
+    private fun updateUI(isConnection: Boolean) {
+        binding.noInternet.isVisible = !isConnection
     }
 
 }
