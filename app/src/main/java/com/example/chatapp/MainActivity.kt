c97b715a579fba8415d88e796ede6c9e7260426b
DudeNamedVictor <greatgomer3@gmail.com>
2023-03-07 20:01:32 +0300

    Add first iteration themes

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.chatapp
 
+import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.ui.globalsettings.KEY_THEME
+import com.example.chatapp.ui.globalsettings.PREFS_NAME
 import com.example.chatapp.utils.ConnectionLiveData
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +18,21 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding
         get() = _binding!!
+    private val sharedPreferences by lazy {
+        getSharedPreferences(
+            PREFS_NAME,
+            Context.MODE_PRIVATE
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
+        changeTheme()
+        initNavigation()
+        checkInternet()
 
         setContentView(binding.root)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        navHostFragment.navController
-        checkInternet()
     }
 
     override fun onDestroy() {
@@ -45,6 +54,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateUI(isConnection: Boolean) {
         binding.noInternet.isVisible = !isConnection
+    }
+
+    private fun changeTheme() {
+        if (sharedPreferences.getInt(KEY_THEME, -1) == R.style.Theme_ChatAppNight) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        setTheme(sharedPreferences.getInt(KEY_THEME, -1))
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
+    fun recreateActivity() {
+        ActivityCompat.recreate(this@MainActivity)
     }
 
 }
--- app/src/main/java/com/example/chatapp/ui/globalsettings/GlobalSettingsFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/globalsettings/GlobalSettingsFragment.kt
@@ -1,18 +1,31 @@
 package com.example.chatapp.ui.globalsettings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.chatapp.MainActivity
+import com.example.chatapp.R
 import com.example.chatapp.databinding.GlobalSettingsFragmentBinding
+
+const val PREFS_NAME = "theme_prefs"
+const val KEY_THEME = "prefs.theme"
 
 class GlobalSettingsFragment : Fragment() {
 
     private var _binding: GlobalSettingsFragmentBinding? = null
     private val binding: GlobalSettingsFragmentBinding
         get() = _binding!!
+    private val sharedPreferences by lazy {
+        activity?.getSharedPreferences(
+            PREFS_NAME,
+            Context.MODE_PRIVATE
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +34,7 @@ class GlobalSettingsFragment : Fragment() {
     ): View {
         _binding = GlobalSettingsFragmentBinding.inflate(inflater, container, false)
         initToolbar()
+        chooseTheme()
 
         return binding.root
     }
@@ -33,6 +47,23 @@ class GlobalSettingsFragment : Fragment() {
     private fun initToolbar() {
         binding.toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun chooseTheme() {
+        binding.theme.apply {
+            redTheme.setOnClickListener { setTheme(R.style.Theme_ChatAppRed) }
+            greenTheme.setOnClickListener { setTheme(R.style.Theme_ChatAppGreen) }
+            blueTheme.setOnClickListener { setTheme(R.style.Theme_ChatAppBlue) }
+            defaultTheme.setOnClickListener { setTheme(R.style.Theme_ChatAppDefault) }
+            nightTheme.setOnClickListener { setTheme(R.style.Theme_ChatAppNight) }
+        }
+    }
+
+    private fun setTheme(theme: Int) {
+        sharedPreferences?.edit {
+            putInt(KEY_THEME, theme).apply()
+            (activity as MainActivity).recreateActivity()
         }
     }
 
